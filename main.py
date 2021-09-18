0bbe157544d1988ec3043b2dc41441703406fcf8
Internetsation <ericbetz.civ@gmail.com>
2021-09-18 19:13:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# This function asks for user name and prints to screen.
+def my_function():
+    # Get user name
+    name = input("Tell me your name:")
+    # Print user name
+    print(f'Hello {name}')
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Why will this not change? please tell me {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('Guido!')
+    my_function()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
